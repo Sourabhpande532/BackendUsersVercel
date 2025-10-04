2a6a07fd5089627b20f489c9a6f480b93ee5f311
sourabhpande532 <sourabhpande43@gmail.com>
2025-10-04 12:04:30 +0530

    doc:add som content

---
+++ track.js
@@ -0,0 +1,22 @@
+/* 
+DEPLOYMENT PROCESS: 
+Initilize with 
+-npm init -y
+-install express cors 
+-create index.js add server code
+-use middlewhere app.use(express+cors)
+-git init (ON Terminal)
+-add gitignore file including befour send code to server add ensure it /node_modules (optionally you can add here PORT,.env all secretes which we don't want to send github) ..
+-git status 
+-git add -A 
+-git commit -m "chore/Feat(Task): xyz"
+-git branch -M main/master
+-git remote add origin url 
+-git push -u origin 
+
+Then go to vercel webisite login 
+-go to project and simply deploy
+-you'll get this domain 
+https://backend-users-vercel-tau.vercel.app/
+now you can use this api on frontend applications use api/users/2
+*/
